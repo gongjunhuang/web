255133b7d08386c8999d3bd3284bd0fa0b869e09
gj <564151075@qq.com>
2018-04-22 22:08:49 +0800

    0422

--- lesson3/routes/__init__.py
+++ lesson3/routes/__init__.py
@@ -1,5 +1,4 @@
 def response_with_headers(headers, status_code=200):
     header = 'HTTP/1.1 {} OK\r\n'.format(status_code)
-    header += ''.join(['{}: {}\r\n'.format(k, v)
-                           for k, v in headers.items()])
+    header += ''.join(['{}: {}\r\n'.format(k, v) for k, v in headers.items()])
     return header
